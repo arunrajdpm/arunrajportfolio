788d5ca463bbf3f133cf275489775d6f420203df
arunrajdpm <arunrajdpm@gmail.com>
2020-01-23 15:43:18 +0530

    For experience update

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -10,16 +10,33 @@
           @mouseleave="item.show = !item.show"
           @click="item.show = !item.show; setChildrens(item.children, item.show) "
         >
-          <v-card class="pa-4 mx-2" color="rgba(255, 0, 0, 0.5)">
-            <v-card-text class="py-4 my-4">{{ item.name }}</v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn text>{{ item.numbers}}</v-btn>
-            </v-card-actions>
+          <v-card class="pa-4 pb-2 mx-2" min-height="190">
+            <v-card-text class="py-2">
+              <div class="text-center">
+                <span class="d-block py-2 pb-3 title">{{ item.name }}</span>
+                <div class="pb-2">
+                  <v-progress-circular
+                    :value="item.knowledge"
+                    :rotate="0"
+                    :width="6"
+                    size="50"
+                    color="light-blue"
+                  >{{ item.knowledge}}%</v-progress-circular>
+                </div>
+                <span class="d-block sub-heading">{{ item.numbers}}</span>
+              </div>
+            </v-card-text>
+
+            <div>
+              <span
+                class="caption d-block text-right"
+                v-if="item.children && item.children.length > 0"
+              >click to see details</span>
+            </div>
           </v-card>
         </v-col>
       </template>
-      <Card-list v-if="childrens.length > 0 && show" :list="childrens"></Card-list>
+      <Card-list v-if="childrens && childrens.length > 0 && show" :list="childrens"></Card-list>
     </v-row>
   </v-container>
 </template>
--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -14,14 +14,37 @@ export default {
       {
         name: "Full Stack Developer",
         numbers: "3.5 Years",
+        knowledge: "55",
         show: false,
         children: [
           {
-            name: "1.6",
+            name: "Java",
+            numbers: "1.5 Years",
+            knowledge: "70",
             show: true,
-            children: [{ name: "1.6" }, { name: "2" }]
+            children: [
+              { name: "Spring Boot", knowledge: "50", numbers: "1.5 Years" }
+            ]
           },
-          { name: "2", show: true, children: [{ name: "1.6" }] }
+          {
+            name: "JavaScript",
+            numbers: "2 Years",
+            knowledge: "75",
+            show: true,
+            children: [
+              { name: "Vue JS", knowledge: "80", numbers: "1 Years" },
+              { name: "Angular JS", knowledge: "80", numbers: "1 Years" },
+              { name: "Angular", knowledge: "55", numbers: "1 Years" },
+              {
+                name: "Node",
+                knowledge: "66",
+                numbers: "6 month",
+                children: [
+                  { name: "Express", knowledge: "50", numbers: "6 month" }
+                ]
+              }
+            ]
+          }
         ]
       }
     ]
